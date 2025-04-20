97a70b423ece2e068cd5bb735c1ac57492d229e9
Gregory Hall <ghall@dataguys.net>
2025-04-20 14:51:17 -0700

    Update deploy-cti.sh

--- scripts/deploy-cti.sh
+++ scripts/deploy-cti.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+#set -euo pipefail
 
 ################################################################################
 # Central Threat Intelligence – unattended deploy script
